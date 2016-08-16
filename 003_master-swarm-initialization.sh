2ea40ceb2c7eec956c359bfdec206861bdf40a71
Patrck.Brs <PatrckBrs@users.noreply.github.com>
2016-08-16 16:16:51 +0200

    Update 003_master-swarm-initialization.sh

--- 003_master-swarm-initialization.sh
+++ 003_master-swarm-initialization.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export TOKEN=$(for i in $(seq 1 32); do echo -n $(echo "obase=16; $(($RANDOM % 16))" | bc); done; echo) 
 
 function getip() { (traceroute $1 2>&1 | head -n 1 | cut -d\( -f 2 | cut -d\) -f 1) }
 
